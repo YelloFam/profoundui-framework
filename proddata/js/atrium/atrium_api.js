158096ae03f8ef96cc1fbfa9fc80260d19607c95
David Russo <drusso@profoundlogic.com>
2015-12-28 16:30:35 -0500

    Replace tabs with spaces in all Atrium files.

--- proddata/js/atrium/atrium_api.js
+++ proddata/js/atrium/atrium_api.js
@@ -22,64 +22,64 @@ if (!window["Atrium"]) window["Atrium"] = new Object();
 
 Atrium["launchURL"] = function(url, title, closable) {
 
-	if (!parent["Atrium"]) return;
-	return parent["Atrium"]["api"]["launchURL"](url, title, closable); 
-	
+  if (!parent["Atrium"]) return;
+  return parent["Atrium"]["api"]["launchURL"](url, title, closable); 
+  
 }
 
 Atrium["closeTab"] = function(tab) {
-	
-	var par = parent.parent || parent;
-	if (!par || !par["Atrium"]) return;
+  
+  var par = parent.parent || parent;
+  if (!par || !par["Atrium"]) return;
 
-	par["Atrium"]["api"]["closeTab"](window, tab);
+  par["Atrium"]["api"]["closeTab"](window, tab);
 
-	
+  
 }
 
 Atrium["launchItem"] = function(itemId, closable, callback) {
 
-	if (!parent["Atrium"]) return;	
-	return parent["Atrium"]["api"]["launchItem"](itemId, closable, callback);
-	
+  if (!parent["Atrium"]) return;  
+  return parent["Atrium"]["api"]["launchItem"](itemId, closable, callback);
+  
 }
 
 Atrium["activateTab"] = function(tab) {
 
-	if (!parent["Atrium"]) return;
-	parent["Atrium"]["api"]["activateTab"](tab);
-	
+  if (!parent["Atrium"]) return;
+  parent["Atrium"]["api"]["activateTab"](tab);
+  
 }
 
 Atrium["closeAllTabs"] = function(keepHomePage) {
-	
-	if (!parent["Atrium"]) return;	
-	parent["Atrium"]["api"]["closeAllTabs"](window, keepHomePage);
-	
+  
+  if (!parent["Atrium"]) return;  
+  parent["Atrium"]["api"]["closeAllTabs"](window, keepHomePage);
+  
 }
 
 Atrium["getCurrentTab"] = function() {
-	
-	if (!parent["Atrium"]) return;
-	return parent["Atrium"]["api"]["getCurrentTab"](window);
-	
+  
+  if (!parent["Atrium"]) return;
+  return parent["Atrium"]["api"]["getCurrentTab"](window);
+  
 }
 
 Atrium["getToken"] = function() {
 
-	var par = parent.parent || parent;
-	if (!par || !par["Atrium"]) return;	
-	
-	return par["Atrium"]["api"]["getToken"]();
+  var par = parent.parent || parent;
+  if (!par || !par["Atrium"]) return;  
+  
+  return par["Atrium"]["api"]["getToken"]();
 
 }
 
 Atrium["getUser"] = function() {
 
-	var par = parent.parent || parent;
-	if (!par || !par["Atrium"]) return;	
-	
-	return par["Atrium"]["api"]["getUser"]();  
+  var par = parent.parent || parent;
+  if (!par || !par["Atrium"]) return;  
+  
+  return par["Atrium"]["api"]["getUser"]();  
 
 }
 
